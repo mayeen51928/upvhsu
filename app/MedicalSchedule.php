d80312c3009e78665700b1a9a4543f9b818446ff
johneim <jsmission@up.edu.ph>
2017-01-16 21:52:31 +0800

    For displaying appointments in patient dashboard

--- app/MedicalSchedule.php
+++ app/MedicalSchedule.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class MedicalSchedule extends Model
 {
     protected $table = "medical_schedules";
+
+    public function medicalappointment()
+    {
+        return $this->hasOne(MedicalAppointment::class, 'medical_schedule_id');
+    }
 }
