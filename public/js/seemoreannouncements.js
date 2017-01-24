07f1641111dd4850bf0b05bc8c1a4d2058663498
Mayenne Joi Catuiran <Mayenne Joi Catuiran>
2017-01-24 22:48:56 +0800

    Show/hide announcement

--- public/js/seemoreannouncements.js
+++ public/js/seemoreannouncements.js
@@ -3,15 +3,20 @@ $(document).ready( function(){
     headers: { 'X-CSRF-Token' : $('meta[name=_token]').attr('content') }
   });
 
-  $('.see_more_announcements').click(function() {
+  $('.see_more_announcement').click(function() {
     id = $(this).attr('id').split("_");
     announcementId = id[3];
     $('#announcement_body_'+ announcementId).css('display','block');
-    $('#see_more_announcements_'+announcementId).removeClass('see_more_announcements').addClass('newClass');
-    $('.newClass').click(function() {
-      console.log("Hide announcement.");
-      $('#announcement_body_'+ announcementId).css('display','none');
-      $('#see_more_announcements_'+announcementId).removeClass('newClass').addClass('see_more_announcements');
-    })
+    $('#hide_announcement_'+ announcementId).css('display','block');
+    $('#see_more_announcement_'+ announcementId).css('display','none');
+  })
+
+  $('.hide_announcement').click(function() {
+    id = $(this).attr('id').split("_");
+    console.log(id);
+    announcementId = id[2];
+    $('#announcement_body_'+ announcementId).css('display','none');
+    $('#hide_announcement_'+ announcementId).css('display','none');
+    $('#see_more_announcement_'+ announcementId).css('display','block');
   })
 })
